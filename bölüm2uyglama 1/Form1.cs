20bc4009cdc2e756532d28f25e723d19e388e20e
akinseman <145451296+akinseman@users.noreply.github.com>
2023-10-03 15:12:18 +0300

    Add files via upload

---
+++ bölüm2uyglama 1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bölüm2uyglama_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 19;
+
+            if(yas < 18)
+            {
+                MessageBox.Show("Üzgünüm eliyet yok.");
+            }
+
+            if(yas >= 18) 
+            {
+                MessageBox.Show("Eliyet alabilisiniz");
+            }
+        }
+    }
+}
